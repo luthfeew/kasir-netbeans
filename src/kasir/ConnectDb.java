ec093522c9e4a0d601d8b4ed6d45ab60f40e4e9e
luthfeew <luthfeew@gmail.com>
2020-12-15 19:20:47 +0700

    add timezone

--- src/kasir/ConnectDb.java
+++ src/kasir/ConnectDb.java
@@ -11,7 +11,6 @@ package kasir;
  */
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 
 public class ConnectDb {        
     static Connection con=null;
@@ -19,13 +18,13 @@ public class ConnectDb {
         if (con != null) return con;
         String db = "kasir";
         String user = "root";
-        String pass = "12345679";
+        String pass = "";
         return getConnection(db, user, pass);
     }
     
     private static Connection getConnection(String db_name,String user_name,String password) {
         try {
-            con=DriverManager.getConnection("jdbc:mysql://localhost/"+db_name+"?user="+user_name+"&password="+password);
+            con=DriverManager.getConnection("jdbc:mysql://localhost/"+db_name+"?serverTimezone=Asia/Jakarta&user="+user_name+"&password="+password);
         }
         catch(Exception e) {
             e.printStackTrace();
