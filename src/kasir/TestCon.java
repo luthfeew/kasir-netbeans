13c0e8e8eba877463a3bf9c89c9defaeb67a9fd7
luthfeew <luthfeew@gmail.com>
2020-12-17 15:58:55 +0700

    cleanup

--- src/kasir/TestCon.java
+++ src/kasir/TestCon.java
@@ -11,8 +11,6 @@ package kasir;
  */
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 public class TestCon {
     public static void main(String args[]){
