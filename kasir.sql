81e467d5d3ddc21f7f268c06a7ca71fd0ae2dcac
luthfeew <luthfeew@gmail.com>
2020-12-25 17:56:52 +0700

    update kasir.sql (workaround)

--- kasir.sql
+++ kasir.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100417
  File Encoding         : 65001
 
- Date: 25/12/2020 13:57:17
+ Date: 25/12/2020 17:55:35
 */
 
 SET NAMES utf8mb4;
@@ -45,6 +45,31 @@ INSERT INTO `barang` VALUES (17, 'C01', 'bakpao', '2500', 'pcs', '-');
 INSERT INTO `barang` VALUES (25, 'A01', 'rendang', '9000', 'pcs', '-');
 
 -- ----------------------------
+-- Table structure for counter
+-- ----------------------------
+DROP TABLE IF EXISTS `counter`;
+CREATE TABLE `counter`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `count` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 48 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of counter
+-- ----------------------------
+INSERT INTO `counter` VALUES (37, 1);
+INSERT INTO `counter` VALUES (38, 1);
+INSERT INTO `counter` VALUES (39, 1);
+INSERT INTO `counter` VALUES (40, 2);
+INSERT INTO `counter` VALUES (41, 2);
+INSERT INTO `counter` VALUES (42, 2);
+INSERT INTO `counter` VALUES (43, 2);
+INSERT INTO `counter` VALUES (44, 3);
+INSERT INTO `counter` VALUES (45, 3);
+INSERT INTO `counter` VALUES (46, 3);
+INSERT INTO `counter` VALUES (47, 3);
+
+-- ----------------------------
 -- Table structure for transaksi
 -- ----------------------------
 DROP TABLE IF EXISTS `transaksi`;
@@ -57,12 +82,19 @@ CREATE TABLE `transaksi`  (
   `total` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `timestamp` timestamp(6) NULL DEFAULT current_timestamp(6) ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 44 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of transaksi
 -- ----------------------------
-INSERT INTO `transaksi` VALUES (1, '0001', 'A71', '2', '7000', '14000', '2020-12-25 13:51:04.587589');
-INSERT INTO `transaksi` VALUES (3, '0001', 'C01', '6', '2500', '15000', '2020-12-25 13:56:47.024295');
+INSERT INTO `transaksi` VALUES (35, '1', 'A69', '1', '5000', '5000', '2020-12-25 17:50:06.085986');
+INSERT INTO `transaksi` VALUES (36, '1', 'A70', '12', '4000', '48000', '2020-12-25 17:50:08.271370');
+INSERT INTO `transaksi` VALUES (37, '1', 'B30', '2', '11000', '22000', '2020-12-25 17:50:11.366151');
+INSERT INTO `transaksi` VALUES (38, '2', 'B69', '1', '10000', '10000', '2020-12-25 17:50:16.629135');
+INSERT INTO `transaksi` VALUES (39, '2', 'C01', '2', '2500', '5000', '2020-12-25 17:50:19.808045');
+INSERT INTO `transaksi` VALUES (40, '2', 'C12', '3', '1000', '3000', '2020-12-25 17:50:23.840021');
+INSERT INTO `transaksi` VALUES (41, '3', 'A72', '1', '1000', '1000', '2020-12-25 17:50:28.638763');
+INSERT INTO `transaksi` VALUES (42, '3', 'B30', '2', '11000', '22000', '2020-12-25 17:50:32.160021');
+INSERT INTO `transaksi` VALUES (43, '3', 'C01', '3', '2500', '7500', '2020-12-25 17:50:36.234068');
 
 SET FOREIGN_KEY_CHECKS = 1;
